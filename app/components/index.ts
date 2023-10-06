f0e24af1b77e7f6b6028d260f226c31d8b10ca0b
Roberto Quintero <langdonhp435@gmail.com>
2023-10-05 19:46:04 -0600

    Configura ruta para agregar usuario Post /api/auth/signup

--- app/components/index.ts
+++ app/components/index.ts
@@ -5,3 +5,6 @@ export {default as RestaurantCard} from './RestaurantCard';
 export {default as SerachBar} from './SerachBar';
 export {default as Price} from './Price';
 export {default as Stars} from './Stars';
+export {default as AuthModal} from './AuthModal';
+export {default as AuthModalInputs} from './AuthModalInputs';
+
